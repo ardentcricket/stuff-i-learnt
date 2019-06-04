fbef1a02efc2b70de0d90f4404c0b0369ef14579
nambiarn <nambiar.nikhil11@gmail.com>
2019-06-04 08:40:47 -0500

    add to the pile

--- js/constructors.js
+++ js/constructors.js
@@ -17,3 +17,26 @@ bruce.fly();
 
 bruce.detect();
 // I can detect mouse and eat it
+
+// When your instance keeps the modification for the 
+// for that instance alone and does not propogate it
+// to other instances
+
+//constructor function
+function Human(name, age) {
+    this.name = name,
+    this.age = age,
+    this.friends = ["blah", "blah1"]
+}
+    
+// Instance generation
+var person1 = new Human("fudges", 23)
+var person2 = new Human("brownies", 24)
+
+// Check yo friends
+console.log(person1.friends)
+console.log(person2.friends)
+
+// got new friends now
+person2.friends.push("ice cream")
+console.log(person2.freinds)
